c111d8f47f506cc55adef570f10fd28388c64e73
snrnapa <shino.satoru@gmail.com>
2024-03-12 21:21:07 +0900

    完成版としてpush

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,16 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
-@RequestMapping("/templates/home") //遷移先を指定
 // Corsの設定をいい感じにする
-@CrossOrigin(origins = "*")
-
+// @CrossOrigin(origins = "*")
 // http://localhost:8080/templates/home
-
-public class HomeController{
-    @GetMapping("/test")
-    public String home(){
-        return "home"; //メソッドの戻り値homeが指定したhtml(home.html)を呼び出す
+@Controller
+@RequestMapping("/home") // 遷移先を指定
+public class HomeController {
+    // @GetMapping("/test")
+    @RequestMapping(method = RequestMethod.GET)
+    public String home() {
+        return "home"; // メソッドの戻り値homeが指定したhtml(home.html)を呼び出す
     }
 }
